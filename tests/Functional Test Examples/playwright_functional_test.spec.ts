719930c196c1b1fbdbe845c8972ae3c7d00177fe
tmorris27 <122841311+tmorris27@users.noreply.github.com>
2025-05-28 15:06:05 -0400

    Playwright config webkit browser update

--- tests/Functional Test Examples/playwright_functional_test.spec.ts
+++ tests/Functional Test Examples/playwright_functional_test.spec.ts
@@ -5,5 +5,15 @@ test('header sign in link functional test', async ({ page }) => {
     await page.goto("https://practicesoftwaretesting.com/");
 
     await page.locator('[data-test="nav-sign-in"]').click();
-    await expect(page).toHaveURL("https://practicesoftwaretesting.com/")
+    await expect(page).toHaveURL("https://practicesoftwaretesting.com/auth/login")
 })
+
+// test of the Categories navigation menu
+test('categories nav menu functional test', async ({ page }) => {
+    await page.goto("https://practicesoftwaretesting.com/");
+    await page.locator('[data-test="nav-categories"]').click();
+    await page.locator('[data-test="nav-hand-tools"]').click();
+    await expect(page).toHaveURL('https://practicesoftwaretesting.com/category/hand-tools')
+    const categoryText = page.locator('[data-test="page-title"]');
+    await expect(categoryText).toHaveText("Category: Hand Tools");
+});

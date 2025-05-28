159af8ffc3d71125f6b16dcc6dd49cf7a65710f8
tmorris27 <122841311+tmorris27@users.noreply.github.com>
2025-05-28 16:20:42 -0400

    Update playwright_visual_tests.spec.ts

--- tests/Visual Test Examples/playwright_visual_tests.spec.ts
+++ tests/Visual Test Examples/playwright_visual_tests.spec.ts
@@ -17,3 +17,11 @@ test('nav menu visual test', async ({ page }) => {
     const navDropdownMenu = page.getByRole('list', { name: 'nav-categories' })
     await expect(navDropdownMenu).toHaveScreenshot('category dropdown menu.png')
 })
+
+// visual test of the header element
+
+test('header visual test', async ({ page }) => {
+    await page.goto("https://practicesoftwaretesting.com/");
+    const header = page.locator('.navbar navbar-expand-lg navbar-light bg-light');
+    await expect(header).toHaveScreenshot('header.png');
+});

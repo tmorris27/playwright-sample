7994ee418ca2a57863e4fb973eab6851c1483abe
tmorris27 <122841311+tmorris27@users.noreply.github.com>
2025-05-28 16:37:36 -0400

    Update playwright_functional_test.spec.ts

--- tests/Functional Test Examples/playwright_functional_test.spec.ts
+++ tests/Functional Test Examples/playwright_functional_test.spec.ts
@@ -17,3 +17,13 @@ test('categories nav menu functional test', async ({ page }) => {
     const categoryText = page.locator('[data-test="page-title"]');
     await expect(categoryText).toHaveText("Category: Hand Tools");
 });
+
+// test of add to cart functionality
+
+test('add to cart functional test', async ({ page }) => {
+    await page.goto("https://practicesoftwaretesting.com/");
+    await page.locator('[data-test="product-01JWC7PDMY1YPD61153G9Q78TY"]').click();
+    await page.locator('[data-test="add-to-cart"]').click();
+    await page.locator('[data-test="nav-cart"]').click();
+    await expect(page.locator('[data-test="product-quantity"]')).toHaveValue('1');
+});

63599a52f08e2dd8202e7656a6e9bcec93299959
tmorris27 <122841311+tmorris27@users.noreply.github.com>
2025-05-29 15:02:59 -0400

    Update playwright_visual_tests.spec.ts

--- tests/Visual Test Examples/playwright_visual_tests.spec.ts
+++ tests/Visual Test Examples/playwright_visual_tests.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 
 // visual test of login page
 
-test('header sign in link visual test', async ({ page }) => {
+test('header sign in page visual test', async ({ page }) => {
     await page.goto("https://practicesoftwaretesting.com/");
     await page.locator('[data-test="nav-sign-in"]').click();
     await expect(page).toHaveScreenshot('login page.png')

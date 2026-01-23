76a363e3911f8339eef21eab49dc69153f12c763
tristan.morris@vml.com <tristan.morris@vml.com>
2026-01-23 14:38:13 -0500

    update to add negative test cases for account creation form

--- tests/PracticeSoftwareTesting.com/PST_functional_test.spec.ts
+++ tests/PracticeSoftwareTesting.com/PST_functional_test.spec.ts
@@ -100,6 +100,45 @@ test('account login functional test',
     await expect(page).toHaveURL('/account');
     await expect(page.locator('[data-test="page-title"]')).toContainText('My account');
 })
+test('account login negative test incorrect email and password',
+     {
+    tag: ['@PST', '@functionalTests' ],
+},
+    async ({ page, isMobile }) => {
+    await page.goto("/");
+    if (isMobile){
+        await page.locator('.navbar-toggler-icon').click()
+        await page.locator('[data-test="nav-sign-in"]').click();
+    } else {
+    await page.locator('[data-test="nav-sign-in"]').click();
+    }
+    await page.locator('[data-test="email"]').click();
+    await page.locator('[data-test="email"]').fill("test@test.com")
+    await page.locator('[data-test="password"]').click();
+    await page.locator('[data-test="password"]').fill('test');
+    await page.locator('[data-test="login-submit"]').click();
+    await expect(page.locator('[data-test="login-error"]')).toHaveText('Invalid email or password');
+});
+
+test('account login negative test invalid email address',
+     {
+    tag: ['@PST', '@functionalTests' ],
+},
+    async ({ page, isMobile }) => {
+    await page.goto("/");
+    if (isMobile){
+        await page.locator('.navbar-toggler-icon').click()
+        await page.locator('[data-test="nav-sign-in"]').click();
+    } else {
+    await page.locator('[data-test="nav-sign-in"]').click();
+    }
+    await page.locator('[data-test="email"]').click();
+    await page.locator('[data-test="email"]').fill("test")
+    await page.locator('[data-test="password"]').click();
+    await page.locator('[data-test="password"]').fill('test');
+    await page.locator('[data-test="login-submit"]').click();
+    await expect(page.locator('[data-test="email-error"]')).toHaveText('Email format is invalid');
+});
 
 // test of checkout functionality
 
